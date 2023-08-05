6ced08c4b1225b5f4dd85363c62ce5c8a553e6ef
Thanh Lo <lohthanh@gmail.com>
2023-08-04 20:47:14 -0700

    separated admin and user, user crud

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -1,61 +1,82 @@
+from datetime import datetime
 from flask_app import app
-from flask import render_template, redirect, request, flash, session
+from flask import render_template, redirect, request, flash, session, url_for
 from flask_app.models.user_model import User
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    if 'user_id' in session:
-        return redirect('/dashboard')
-    return render_template('index.html')
+    if "user_id" in session:
+        return redirect("/dashboard")
+    return render_template("index.html")
+
 
 @app.route("/users/register", methods=["POST"])
 def register():
     if not User.validate_data(request.form):
-        return redirect('/')
-    hashed_pass = bcrypt.generate_password_hash(request.form['password'])
-    data = {
-        **request.form,
-        'password' : hashed_pass,
-        'confirm_password' : hashed_pass
-    }
+        return redirect("/")
+    hashed_pass = bcrypt.generate_password_hash(request.form["password"])
+    data = {**request.form, "password": hashed_pass, "confirm_password": hashed_pass}
     logged_user_id = User.create(data)
-    session['user_id'] = logged_user_id
-    return redirect('/dashboard')
+    session["user_id"] = logged_user_id
+    return redirect("/dashboard")
 
-@app.route('/users/login', methods=['POST'])
+
+@app.route("/users/login", methods=["POST"])
 def login_user():
-    data = {
-        'email' : request.form ['email']
-    }
+    data = {"email": request.form["email"]}
     potential_user = User.get_by_email(data)
-    if not potential_user:
-        flash ('Invalid credentials', 'login')
-        print('User Not Found')
-        return redirect ('/')
-    if not bcrypt.check_password_hash(potential_user.password, request.form['password']):
-        flash ('Invalid credentials', 'login')
-        print ('Invalid Password')
-        return redirect ('/')
-    session['user_id'] = potential_user.id
-    return redirect ('/dashboard')
 
-@app.route('/dashboard')
+    if not potential_user:
+        flash("Invalid credentials", "login")
+        print("User Not Found")
+        return redirect("/")
+    
+    if not bcrypt.check_password_hash(
+        potential_user.password, request.form["password"]
+    ):
+        flash("Invalid credentials", "login")
+        print("Invalid Password")
+        return redirect("/")
+    
+    session["user_id"] = potential_user.id
+    User.update_last_logged_on(potential_user.id)
+    print(potential_user.id)
+    return redirect("/dashboard")
+
+
+@app.route("/dashboard")
 def dashboard():
-    if 'user_id' not in session:
-        return redirect('/')
-    data = {
-        'id' : session['user_id']
-    }
+    if "user_id" not in session:
+        return redirect("/")
+    data = {"id": session["user_id"]}
     logged_user = User.get_by_id(data)
     if not logged_user:
-        return redirect('/')
-    all_users = User.get_all_users(data)
-    return render_template('dashboard.html', logged_user = logged_user, all_users=all_users)
+        return redirect("/")
+    all_users = User.get_all_users()
+    return render_template(
+        "dashboard.html", logged_user=logged_user, all_users=all_users
+    )
 
-@app.route('/users/logout')
+
+@app.route("/users/<int:id>/view")
+def view_info(id):
+    session_data = {"id": session["user_id"]}
+    logged_user = User.get_by_id(session_data)
+    if not logged_user:
+        return redirect("/")
+    data = {"id": id}
+    one_user = User.get_one_user(data)
+    last_logged_on = one_user.get_last_logged_on()
+    return render_template(
+        "info.html", one_user=one_user, last_logged_on=last_logged_on, logged_user=logged_user
+    )
+
+
+@app.route("/users/logout")
 def logout():
-    del session['user_id']
-    return redirect('/')
+    del session["user_id"]
+    return redirect("/")
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -1,121 +1,140 @@
+from datetime import datetime
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DB
 import re
 from flask import flash
 
 
-
 ALPHA = re.compile(r"^[a-zA-Z]+$")
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-ALPHANUMERIC = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$")
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
+ALPHANUMERIC = re.compile(
+    r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+)
+
 
 class User:
     def __init__(self, data):
-        self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']
-        self.user_level = data['user_level']
-        self.employee_id = data['employee_id']
-        self.password = data['password']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-    
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.user_level = data["user_level"]
+        self.employee_id = data["employee_id"]
+        self.password = data["password"]
+        self.last_logged_on = data["last_logged_on"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
     @classmethod
-    def create(cls, data): #same as save()
+    def create(cls, data):  # same as save()
         query = """INSERT INTO users (first_name, last_name, email, user_level, employee_id, password)
-                VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(user_level)s, %(employee_id)s, %(password)s );"""
+                VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(user_level)s, %(employee_id)s, %(password)s);"""
         results = connectToMySQL(DB).query_db(query, data)
         return results
-    
+
     @classmethod
     def get_by_id(cls, data):
         query = """SELECT * FROM users
                 WHERE id = %(id)s;"""
         results = connectToMySQL(DB).query_db(query, data)
         if results:
-            return cls (results[0])
+            return cls(results[0])
         return False
-    
+
     @classmethod
     def get_by_email(cls, data):
         query = """SELECT * FROM users
                 WHERE email = %(email)s;"""
         results = connectToMySQL(DB).query_db(query, data)
         if results:
-            return cls (results[0])
+            return cls(results[0])
         return False
-    
+
     @classmethod
-    def get_all_users(cls, data):
+    def get_all_users(cls):
         query = """SELECT * FROM users;"""
-        results = connectToMySQL(DB).query_db(query, data)
-        if results:
-            return cls (results[0])
-        return False
-    
+        results = connectToMySQL(DB).query_db(query)
+        all_users = []
+        for user in results:
+            all_users.append(cls(user))
+        return all_users
+
     @property
     def is_admin(self):
         return self.user_level == 9
-    
+
     @classmethod
-    def get_by_user_level(cls, data):
-        query = """SELECT * FROM users WHERE user_level = %(user_level)s;"""
+    def get_one_user(cls, data):
+        query = """SELECT * FROM users
+                WHERE id = %(id)s"""
         results = connectToMySQL(DB).query_db(query, data)
         if results:
-            return cls (results[0])
-        return False
-
+            one_user = cls(results[0])
+            return one_user
+        return results
+    
+    @classmethod
+    def update_last_logged_on(cls, id):
+        query = """UPDATE users SET last_logged_on = NOW() WHERE id = %(id)s;"""
+        data = {"id" : id}
+        results = connectToMySQL(DB).query_db(query, data)
+        print(data)
+        print(results)
+        return results
+    
+    def get_last_logged_on(self):
+        return self.last_logged_on
 
     @staticmethod
-    def validate_data (data):
+    def validate_data(data):
         is_valid = True
-        if len(data['first_name']) < 1:
+        if len(data["first_name"]) < 1:
             is_valid = False
-            flash ('First name required', 'registration')
-        elif len(data['first_name']) < 2:
+            flash("First name required", "registration")
+        elif len(data["first_name"]) < 2:
             is_valid = False
-            flash ('First name must be at least 2 characters', 'registration')
-        elif not ALPHA.match(data['first_name']):
+            flash("First name must be at least 2 characters", "registration")
+        elif not ALPHA.match(data["first_name"]):
             is_valid = False
-            flash ('First name must be letters only', 'registration')
-        if len(data['last_name']) < 1:
+            flash("First name must be letters only", "registration")
+        if len(data["last_name"]) < 1:
             is_valid = False
-            flash ('Last name required', 'registration')
-        elif len(data['last_name']) < 2:
+            flash("Last name required", "registration")
+        elif len(data["last_name"]) < 2:
             is_valid = False
-            flash ('Last name must be at least 2 characters', 'registration')
-        elif not ALPHA.match(data['last_name']):
+            flash("Last name must be at least 2 characters", "registration")
+        elif not ALPHA.match(data["last_name"]):
             is_valid = False
-            flash ('Last name must be letters only', 'registration')
-        if len(data['email']) < 1:
+            flash("Last name must be letters only", "registration")
+        if len(data["email"]) < 1:
             is_valid = False
-            flash('Email required', 'registration')  #do email at the end
-        elif not EMAIL_REGEX.match(data['email']):
+            flash("Email required", "registration")  # do email at the end
+        elif not EMAIL_REGEX.match(data["email"]):
             is_valid = False
-            flash('Email must be a valid format', 'registration')
+            flash("Email must be a valid format", "registration")
         else:
-            user_data = {
-                'email' : data['email']
-            }
-            potiential_user = User.get_by_email(user_data)
-            if potiential_user:
-                flash ('Email already exists!', 'registration')
+            user_data = {"email": data["email"]}
+            potential_user = User.get_by_email(user_data)
+            if potential_user:
+                flash("Email already exists!", "registration")
                 is_valid = False
-        if len(data['password']) < 1:
+        if len(data["password"]) < 1:
             is_valid = False
-            flash ('Password required', 'registration')
-        elif len(data['password']) < 3:
+            flash("Password required", "registration")
+        elif len(data["password"]) < 3:
             is_valid = False
-            flash ('Password must be at least 3 characters', 'registration')
-        elif data['password'] != data['confirm_password']:
+            flash("Password must be at least 3 characters", "registration")
+        elif data["password"] != data["confirm_password"]:
             is_valid = False
-            flash ('Password does not match!', 'registration')
-        elif not ALPHANUMERIC.match(data['password']):
+            flash("Password does not match!", "registration")
+        elif not ALPHANUMERIC.match(data["password"]):
             is_valid = False
-            flash('Password must have at least one number and one Uppercase letter', 'registration')
-        if data['user_level'] == '-1':
+            flash(
+                "Password must have at least one number and one Uppercase letter",
+                "registration",
+            )
+        if data["user_level"] == "-1":
             is_valid = False
-            flash ('Please select a user level', 'registration')
-        
+            flash("Please select a user level", "registration")
+
         return is_valid
